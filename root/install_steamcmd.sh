f3dd4da94eee58ade13523976471973ee5803142
Nathan Freeman <natethegreat68.dev@gmail.com>
2018-07-02 11:44:23 -0400

    Initial install_steamcmd.sh script

---
+++ root/install_steamcmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+# Make the directory
+mkdir -p /steam/steamcmd
+
+# Get and extract, then delete the official steamcmd tarball
+wget -P /tmp http://media.steampowered.com/client/steamcmd_linux.tar.gz
+tar -xzf /tmp/steamcmd_linux.tar.gz -C /steam/steamcmd
+rm /tmp/steamcmd_linux.tar.gz
