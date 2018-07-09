d46050d357ba5fbb2e7ee56a98d70748031fb91f
Nathan Freeman <natethegreat68.dev@gmail.com>
2018-07-08 22:49:38 -0400

    Now loop on steamcmd login failures, with delay
    Any login failure from steamcmd will now result in a loop until either there's success or a different error occurs.
    Loop is immediate if no login has been achieve before (maybe a typo in the password or something).
    Loop is 30+/-5 seconds if a login has been achieved before (multiple servers starting up simultaneously, etc.).
    
    closes #4

--- scripts/app_update_and_run.sh
+++ scripts/app_update_and_run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #
 
-# Exit when any command fails
-set -e
-
 # Validate the app's files?
 if [ "$VALIDATE_APP" = "always" ]; then 
     VALIDATE="validate"
@@ -16,19 +13,45 @@ else
 fi
 
 # Update the app by building up a steamcmd command
-/steamcmd/steamcmd.sh \
-    $STEAMCMD_VARIABLES \
-    +login "$STEAM_LOGIN" \
-    +force_install_dir /app \
-    +app_update $APP_ID $VALIDATE \
-    +quit
+# Emulate a do...while loop to check for a login failure
+# We want to continue attempting to login until success or another error code
+while : ; do
+    /steamcmd/steamcmd.sh \
+        $STEAMCMD_VARIABLES \
+        +login "$STEAM_LOGIN" \
+        +force_install_dir /app \
+        +app_update $APP_ID $VALIDATE \
+        +quit
+    retval=$?
+
+    if [ "$retval" -eq 0 ] ; then
+    # Break out of the loop and continue on if retval is 0
+        touch "/$HOME/.logged_in_once"
+        break
+    elif [ "$retval" -ne 5 ] ; then
+    # Exit the whole script if retval is not 5 (login error)
+        exit $retval
+    elif [ -f "/$HOME/.logged_in_once" ] ; then
+    # We've managed to log in at once before,
+    # So the problem probably isn't related to login information,
+    # But rather a collision - multiple servers trying to login at once
+    # Announce that you're looping and sleep for 30 +/- 5 seconds
+    # The random time is to hopefully prevent collisions between login attempts
+        sleep_time=$(( 25 + RANDOM % 11 ))
+        echo "steamcmd login failed; waiting ${sleep_time}s and trying again."
+        sleep "${sleep_time}s"
+    else
+    # Nothing to do here; retry the login immediately
+        echo "steamcmd login failed; try again immediately."
+    fi
+done
 
 # Handle the validation files
 if [ "$VALIDATE_APP" = "once" ]; then
-    touch /app/.validated
+    touch /app/.validated || exit $?
 elif [ "$VALIDATE_APP" = "never" ] && [ -f /app/.validate_once ]; then
-    rm /app/.validate_once
+    rm /app/.validate_once || exit $?
 fi
 
 # Run the thing!
-$APP_EXEC
+$APP_EXEC || exit $?
